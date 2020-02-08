4936d5748b11cc68442c93425730adbb2781fe5d
Abhay Khanka <58368777+abhayKRak3n@users.noreply.github.com>
2020-02-08 18:36:27 +0000

    Add files via upload

---
+++ Assignment2_OpenFlow/CONSTANTS.java
@@ -0,0 +1,23 @@
+
+class CONSTANTS {
+    static final int STARTROUTER = 50000;	//ROUTER ADDRESSES 
+
+    static final int STARTENDUSER = 51000;	//ENDUSER ADDRESS
+
+    static final int CONTROLLERPORT = 52000;	//CONTROLLER ADDRESS 
+
+    static final int ROUTERS = 5;
+
+    static final int ENDUSERS = 2;
+
+    static final String HELLO = "hello";	
+    static final String NEXT = "next";
+
+    static final String DSTHOST = "127.0.0.1";
+
+    static final int HEADER_LENGTH = 2;
+    static final int TYPE_POS = 0;		//type of data 
+    static final int LENGTH_POS = 1;
+
+    static final int STRING_TYPE = 1;
+}
